586e26d871b8dbda72e57ef0a0cf9963f50b9dea
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-08-11 19:21:55 +0530

    cuscoi: Add X and Y offsets
    - Stop overriding time on the camera cutout

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,3 +157,5 @@ TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
 TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
+TW_Y_OFFSET := 120
+TW_H_OFFSET := -120
