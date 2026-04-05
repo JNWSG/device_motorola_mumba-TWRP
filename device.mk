24b36ba6315f7d3db04b81d21863d414b39f197f
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-04-05 11:19:56 +0530

    Update device.mk For A16

--- device.mk
+++ device.mk
@@ -24,21 +24,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Configure twrp common.mk
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# some OrangeFox-specific settings
-$(call inherit-product, $(DEVICE_PATH)/fox_peridot.mk)
-
 PRODUCT_PACKAGES += \
     bootctrl.xiaomi_sm8635.recovery \
     android.hardware.boot@1.2-impl-qti.recovery
 
 # API
-PRODUCT_SHIPPING_API_LEVEL  := 34
-PRODUCT_TARGET_VNDK_VERSION := 34
-BOARD_SHIPPING_API_LEVEL := 34
-SHIPPING_API_LEVEL := 34
+PRODUCT_SHIPPING_API_LEVEL  := 35
+PRODUCT_TARGET_VNDK_VERSION := 35
+BOARD_SHIPPING_API_LEVEL := 35
+SHIPPING_API_LEVEL := 35
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Required modules
+TWRP_REQUIRED_MODULES += \
+    prebuilt
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
