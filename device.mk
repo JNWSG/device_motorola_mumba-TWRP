2adc0cbcebcbb0cd1b932fa7edc24129a0b04f48
suchit-7x <gtsuchit7x@gmail.com>
2026-05-08 12:30:39 +0530

    package: librecovery_updater

--- device.mk
+++ device.mk
@@ -41,5 +41,11 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 TWRP_REQUIRED_MODULES += \
     moto_prebuilt
 
+# FIX: Copy librecovery_updater directly into recovery system/lib64
+# boot-service.qti.recovery needs it via libboot_control_qti.so
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/recovery/root/vendor/lib64/librecovery_updater.so:recovery/root/system/lib64/librecovery_updater.so \
+    $(DEVICE_PATH)/recovery/root/vendor/lib64/librecovery_updater_msm.so:recovery/root/system/lib64/librecovery_updater_msm.so
+    
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
